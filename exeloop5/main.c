e56cb4251662398eb24048c1861fed528ce77609
anasa <quenjelanas@gmail.com>
2025-09-04 12:44:46 +0200

    loop5

---
+++ exeloop5/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num, exp,result = 1;
+
+    printf("Enter the main number:\n");
+    scanf("%d", &num);
+
+    printf("Enter the exposant number:\n");
+    scanf("%d", &exp);
+
+    if (exp == 0){
+        printf("The result is : %d\n", result);
+    }
+    for(int i = 0; i < exp; i++){
+        result = result * num;
+    }
+    printf("Your result is: %d\n", result);
+
+return 0;
+
+}
